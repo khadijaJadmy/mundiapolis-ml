2ec3ab99275b2e74557e32d8bb76f598b1259f57
khadija <68543421+khadijaJadmy@users.noreply.github.com>
2021-03-25 15:10:51 +0100

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -1,1 +1,17 @@
 
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron:
+
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise TypeError("nx must be a positive integer")
+
+        self.b = 0
+        self.W = np.random.randn(nx).reshape(1, nx)
+        self.A = 0
