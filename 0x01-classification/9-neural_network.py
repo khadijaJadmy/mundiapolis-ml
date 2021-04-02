6841016cca127b6372823e9183ab2d2dfe1cd598
khadija <68543421+khadijaJadmy@users.noreply.github.com>
2021-04-02 10:26:59 +0100

    Create 9-neural_network.py

---
+++ 0x01-classification/9-neural_network.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class NeuralNetwork:
+
+    def __init__(self, nx, nodes):
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nx).reshape(1, nx)
+        self.__b1 = 0
+        self.__A1 = 0
+        self.__W2 = np.random.randn(nx).reshape(1, nx)
+        self.__b2 = 0
+        self.__A2 = 0
+
+        @property
+        def b1(self):
+            return self.__b1
+
+        @property
+        def A1(self):
+            return self.__A1
+
+        @property
+        def W1(self):
+            return self.__W1
+
+        @property
+        def b2(self):
+            return self.__b2
+
+        @property
+        def A2(self):
+            return self.__A2
+
+        @property
+        def W2(self):
+            return self.__W2
+
+        
