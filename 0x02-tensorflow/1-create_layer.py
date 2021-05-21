f643a13ed0bdb2727ee98ab2306e3690e873ce64
khadija <68543421+khadijaJadmy@users.noreply.github.com>
2021-05-21 09:25:15 +0100

    Create 1-create_layer.py

---
+++ 0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+
+def create_layer(previous, n, activation):
+  
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init,
+                            name="layer")
+    return layer(previous)
